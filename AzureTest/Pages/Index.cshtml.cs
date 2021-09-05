5b87e8441b35e6547843fc4edea39d094e2c9854
Viktor Levak <levakviktor8@gmail.com>
2021-09-05 13:44:04 +0300

    Database logic added

--- AzureTest/Pages/Index.cshtml.cs
+++ AzureTest/Pages/Index.cshtml.cs
@@ -1,25 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+
 
 namespace AzureTest.Pages
 {
 	public class IndexModel : PageModel
 	{
 		private readonly ILogger<IndexModel> _logger;
+		private readonly IConfiguration _config;
 
-		public IndexModel(ILogger<IndexModel> logger)
+		public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
 		{
 			_logger = logger;
+			_config = configuration;
 		}
 
 		public void OnGet()
 		{
+			string connString = this._config.GetConnectionString("DefaultConnection");
 
+			using (var conn = new SqlConnection(connString))
+			{
+				conn.Open();
+				var cmd = new SqlCommand("SELECT * FROM AzureHelloTable", conn);
+				var dataReader = cmd.ExecuteReader();
+				while (dataReader.Read())
+				{
+					ViewData["messageFromDB"] += "<br/>" + (string)dataReader.GetValue(1);
+				}
+			}
 		}
 	}
 }
